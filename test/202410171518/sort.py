c9e826424b2392bb246e17f168fb66b70b07912d
Chun-Cheng Lin <clin105@mySeneca.ca>
2024-10-17 16:59:03 -0400

    [test][202410171518] submit merge sort

--- test/202410171518/sort.py
+++ test/202410171518/sort.py
@@ -4,7 +4,6 @@ def bubble_sort(l):
       if l[j] > l[j+1]:
         l[j+1], l[j] = l[j], l[j+1]
         
-
 def selection_sort(l):
   for i in range(0, len(l)-1):
     pivot = i
@@ -14,8 +13,6 @@ def selection_sort(l):
     if pivot != i:
       l[pivot], l[i] = l[i], l[pivot]
       
-      
-      
 def insertion_sort(l):
   for i in range(1, len(l)):
     cur = l[i]
@@ -24,9 +21,42 @@ def insertion_sort(l):
       l[j] = l [j-1]
       j-=1
     l[j] = cur
+  
+def merge(l, low, mid, high, new_l):
+  a = low
+  b = mid
+  final = a
+  while (a < mid) and (b < high):
+    if l[a] < l[b]:
+      new_l[final] = l[a]
+      a+=1
+      final+=1
+    else:
+      new_l[final] = l[b]
+      a+=1
+      final+=1
+
+  while a < mid:
+      new_l[final] = l[a]
+      a+=1
+      final+=1
+  while b < high:
+      new_l[final] = l[b]
+      a+=1
+      final+=1
+  for i in range(l):
+    l[i] = new_l[i]
+  
+def rec_merge_sort(l, low, high, new_l):
+  if low < high:
+    mid = (low+high) //2
+    rec_merge_sort(l, low, mid, new_l)
+    rec_merge_sort(l, mid+1, high, new_l)
+    merge(l, low, mid, high, new_l)
     
 def merge_sort(l):
-  pass
+  new_l = [0]*len(l)
+  rec_merge_sort(l, 0, len(l)-1, new_l)
 
 def quick_sort(l):
   pass
